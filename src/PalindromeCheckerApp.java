698fb2c93196d62b7692f170e53ee088e20569b7
Satweek-programme <satweekkumarks09@gmail.com>
2026-03-09 15:00:19 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,23 @@
-public class PalindromeCheckerApp {
+public class PalindromeCheckerApp {public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("=================================");
+        System.out.println("      Palindrome Checker App     ");
+        System.out.println("=================================");
+
+        // Display application details
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Application Version : 1.0");
+
+        // Welcome message
+        System.out.println("\nWelcome to the Palindrome Checker  Management System");
+        System.out.println("Application Version : 1.0");
+
+        // End message for UC1
+        System.out.println("\nSystem Initialized successfully");
+    }
+}
 }
